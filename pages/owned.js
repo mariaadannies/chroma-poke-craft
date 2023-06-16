5e66798c84899163deeb712a01975057d079b80b
mariaadannies <mariaadannies26@gmail.com>
2023-06-16 21:47:06 +0700

    implement owned

--- pages/owned.js
+++ pages/owned.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const OwnedPokemon = () => {
+  const [ownedPokemon, setOwnedPokemon] = useState([]);
+
+  const handleRelease = (pokemonName) => {
+    setOwnedPokemon((prevOwnedPokemon) =>
+      prevOwnedPokemon.filter((pokemon) => pokemon !== pokemonName)
+    );
+  };
+
   return (
     <div>
       <h1>Owned Pokemon</h1>
-      {/* Display owned pokemons */}
+      <ul>
+        {ownedPokemon.length === 0 ? (
+          <li>No owned pokemon.</li>
+        ) : (
+          ownedPokemon.map((pokemon) => (
+            <li key={pokemon}>
+              {pokemon}{' '}
+              <button onClick={() => handleRelease(pokemon)}>Release</button>
+            </li>
+          ))
+        )}
+      </ul>
     </div>
   );
 };
