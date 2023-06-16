5a09aa2cefa5903f34d3ed88c14ab26c02ad8dcd
mariaadannies <mariaadannies26@gmail.com>
2023-06-17 00:00:51 +0700

    fetch all data

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import axios from 'axios';
 
 const Home = () => {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPokemonList = async () => {
+      try {
+        const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=20');
+        const results = response.data.results;
+        const pokemonDataPromises = results.map(async (pokemon) => {
+          const response = await axios.get(pokemon.url);
+          return response.data;
+        });
+        const pokemonData = await Promise.all(pokemonDataPromises);
+        setPokemonList(pokemonData);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching pokemon list:', error);
+      }
+    };
+
+    fetchPokemonList();
+  }, []);
+
   return (
     <div>
       <h1>Pokemon List</h1>
-      <ul>
-        <li>
-          <Link href="/pokemon/1">
-            Bulbasaur
-          </Link>
-        </li>
-        <li>
-          <Link href="/pokemon/4">
-            Charmander
-          </Link>
-        </li>
-        <li>
-          <Link href="/pokemon/7">
-            Squirtle
-          </Link>
-        </li>
-        {/* Add more pokemons */}
-      </ul>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {pokemonList.map((pokemon) => (
+            <li key={pokemon.id}>
+              <Link href={`/pokemon/${pokemon.id}`}>
+                {pokemon.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
